84b41cb067357e586fec3b7f4efb93e632fd2515
thalyssonNascimento <thalyssonwg.nascimento@gmail.com>
2017-05-28 20:30:33 -0300

    Srping Boot- Configuração inicial do Spring Boot com o Tomcat utilizando a anotação @SpringBootApplication

---
+++ src/main/java/com/springboot/java/boot/BootWebApp.java
@@ -0,0 +1,13 @@
+package com.springboot.java.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootWebApp.class, args);
+	}
+	
+}
